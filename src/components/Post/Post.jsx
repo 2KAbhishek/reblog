3a7c5e4072476cc989f2f34ecc0f48e6d3ac2242
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-10-20 18:21:32 +0530

    Rename data

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Post.css';
-const Post = ({post: {title, body, imgUrl}, index}) => {
+const Post = ({post: {title, description, imgUrl}, index}) => {
     return (
         <a href={process.env.PUBLIC_URL + "articles/"+ index + ".html"}>
             <div className='post-container'>
                 <h1 className='heading'>{title}</h1>
                 <img className='image' src={imgUrl} alt='post' />
-                <p className='content'>{body.substr(0,128)}</p>
+                <p className='content'>{description.substr(0,32)}</p>
             </div>
             <br />
         </a>
